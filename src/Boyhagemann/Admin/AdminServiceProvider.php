ac646d705d9cfb6d7450bdf8fe7e9a5a7cf01246
Boy Hagemann <boy@swis.nl>
2013-08-02 00:27:43 +0200

    initial commit

---
+++ src/Boyhagemann/Admin/AdminServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Boyhagemann\Admin;
+
+use Illuminate\Support\ServiceProvider;
+use Route;
+
+class AdminServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{            
+            $this->package('admin', 'admin');
+            
+            $this->app->register('Boyhagemann\Pages\PagesServiceProvider');
+//            $this->app->register('Boyhagemann\Navigation\NavigationServiceProvider');   
+	}
+
+        public function boot()
+        {             
+            Route::get('admin/resources/import/{class}', 'Boyhagemann\Admin\Controller\ResourceController@import')->where('class', '(.*)');
+            Route::get('admin/resources/scan', 'Boyhagemann\Admin\Controller\ResourceController@scan');
+            Route::resource('admin/resources', 'Boyhagemann\Admin\Controller\ResourceController');
+        }
+        
+        /**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
---
+++ src/Boyhagemann/Admin/Controller/ResourceController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Boyhagemann\Admin\Controller;
+
+use Boyhagemann\Crud\CrudController;
+use Boyhagemann\Form\FormBuilder;
+use Boyhagemann\Model\ModelBuilder;
+use Boyhagemann\Overview\OverviewBuilder;
+use Route, View;
+
+class ResourceController extends CrudController
+{
+
+    public function scan()
+    {
+        $scanner = new \Boyhagemann\Crud\Scanner;
+        $controllers = $scanner->scanForControllers(array('../app/controllers', '../workbench', '../vendor'));
+        
+        return View::make('admin::resource/scan', compact('controllers'));
+    }
+    
+    public function import($class)
+    {
+        $controller = $this->getController($class);  
+        $this->formBuilder->get('title')->value($controller->getModelBuilder()->getName());
+        $this->formBuilder->get('controller')->value(get_class($controller));
+        
+        $form = $this->getForm();
+        $model = $this->getModel();
+        $route = 'admin.resources';
+        
+        return View::make('crud::crud/create', compact('form', 'model', 'route'));
+    }
+
+    /**
+     * @param FormBuilder $fb
+     */
+    public function buildForm(FormBuilder $fb)
+    {
+        $fb->text('title')->label('Title');
+        $fb->text('url')->label('Url');
+        $fb->text('controller')->label('Controller');
+    }
+
+    /**
+     * @param ModelBuilder $mb
+     */
+    public function buildModel(ModelBuilder $mb)
+    {
+        $mb->name('Admin\Resource')->table('resources');
+    }
+
+    /**
+     * @param OverviewBuilder $ob
+     */
+    public function buildOverview(OverviewBuilder $ob)
+    {
+        $ob->fields(array('title', 'url', 'controller'));
+    }
+
+
+    /**
+     * 
+     * @param string $key
+     * @return CrudController
+     */
+    protected function getController($key)
+    {
+        $class = str_replace('/', '\\', $key);
+        return \App::make($class);        
+    }
+
+}
+
