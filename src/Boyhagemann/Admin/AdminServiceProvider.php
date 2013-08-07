911c112e25fc87f56944c3153f0c83e662d0ee99
boyhagemann <boy@swis.nl>
2013-08-07 09:56:57 +0200

    fix install

--- src/Boyhagemann/Admin/AdminServiceProvider.php
+++ src/Boyhagemann/Admin/AdminServiceProvider.php
@@ -1,7 +1,7 @@
 <?php namespace Boyhagemann\Admin;
 
 use Illuminate\Support\ServiceProvider;
-use Route, View, Artisan, Schema;
+use Route, View, Artisan, Schema, Config, Redirect;
 
 class AdminServiceProvider extends ServiceProvider {
 
@@ -18,29 +18,45 @@ class AdminServiceProvider extends ServiceProvider {
 	 * @return void
 	 */
 	public function register()
-	{            
+	{
+		if(Config::get('database.connections.mysql.database') == 'database') {
+			var_dump('No database set'); exit;
+		}
             $this->package('Boyhagemann\Admin', 'admin');
             
             $this->app->register('Boyhagemann\Pages\PagesServiceProvider');
-            $this->app->register('Boyhagemann\Navigation\NavigationServiceProvider');  
+            $this->app->register('Boyhagemann\Navigation\NavigationServiceProvider');
 	}
 
         public function boot()
-        {    
-            Route::get('admin', function() {
-                return View::make('admin::index.index');
-            });
+        {
+			Route::get('admin/resources/import/{class}', 'Boyhagemann\Admin\Controller\ResourceController@import')->where('class', '(.*)');
+			Route::get('admin/resources/scan', 'Boyhagemann\Admin\Controller\ResourceController@scan');
+
+			Route::get('admin', function() {
+				return View::make('admin::index.index');
+			});
+
+			if(Schema::hasTable('resources')) {
+				foreach(\Boyhagemann\Admin\Model\Resource::get() as $resource) {
+					Route::resource($resource->url, $resource->controller);
+				}
+			}
 
 
-	    if(Schema::hasTable('resources')) {
-	        //foreach(\Admin\Resource::get() as $resource) {
-		    //Route::resource($resource->url, $resource->controller);
-	        //}
-	    }
+			Route::filter('installed', function() {
 
-            
-            Route::get('admin/resources/import/{class}', 'Boyhagemann\Admin\Controller\ResourceController@import')->where('class', '(.*)');
-            Route::get('admin/resources/scan', 'Boyhagemann\Admin\Controller\ResourceController@scan');         
+
+				if(!Schema::hasTable('resources')) {
+
+					Artisan::call('admin:install');
+
+					Redirect::to('admin');
+				}
+
+			});
+
+			Route::when('admin/*', array('installed', 'blocks'));
         }
         
         /**
