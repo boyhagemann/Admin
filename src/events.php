d586545a6bfe9e31c56a78ba0471ced0e3d77035
boyhagemann <boy@swis.nl>
2013-08-08 13:29:03 +0200

    update event

--- src/events.php
+++ src/events.php
@@ -1,9 +1,15 @@
 <?php
 
-Event::listen('formBuilder.buildElement.post', function($name, $element, $factory, $formBuilder) {
+use Boyhagemann\Form\FormBuilder;
+
+Event::listen('formBuilder.buildElement.post', function($name, $element, $factory, FormBuilder $formBuilder) {
 
 	if($formBuilder->getName() == 'Boyhagemann\Pages\Controller\PageController' && $element->getName() == 'method') {
-		$formBuilder->modelSelect('resource_id')->alias('resource')->label('Resource')->model('Boyhagemann\Admin\Model\Resource');
+		$formBuilder->modelSelect('resource_id')
+			->alias('resource')
+			->label('Resource')
+			->model('Boyhagemann\Admin\Model\Resource')
+			->blank('-- No resource --');
 	}
 
 });
