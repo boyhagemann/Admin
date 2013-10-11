25ee3f471b4a88bc1c6cb8a4640b776854a32e04
= <=>
2013-10-11 11:39:49 +0200

    update view

--- src/views/index/dashboard.blade.php
+++ src/views/index/dashboard.blade.php
@@ -1,7 +1,7 @@
 
-<ul class="list-group">
+<ul class="dasboard">
 	@foreach($apps as $app)
-	<li class="list-group-item">
+	<li class="dasboard-item">
 		<a href="{{ URL::route($app['route']) }}"><span class="{{{ $app['icon_class'] }}}"></span><span>{{{ $app['title'] }}}</span></a>
 	</li>
 	@endforeach
