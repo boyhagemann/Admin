cba2e21bab8bc8a5464b5b6291e8434ec2b69772
= <=>
2013-11-15 17:20:16 +0100

    added event

---
+++ src/Boyhagemann/Admin/Subscriber/SwitchContentMode.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Boyhagemann\Admin\Subscriber;
+
+use Illuminate\Events\Dispatcher as Events;
+use Route, Request, Session, Redirect;
+
+class SwitchContentMode
+{
+	/**
+	 * Register the listeners for the subscriber.
+	 *
+	 * @param Events $events
+	 */
+	public function subscribe(Events $events)
+	{
+
+		Route::before(function() {
+
+			switch(Request::get('mode')) {
+
+				case 'view':
+					Session::put('mode', 'view');
+					return Redirect::refresh();
+					break;
+
+				case 'content':
+					Session::put('mode', 'content');
+					return Redirect::refresh();
+					break;
+			}
+
+			if(!Session::get('mode')) {
+				Session::put('mode', 'view');
+			}
+
+		});
+
+	}
+
+
+
+}
--- src/views/navigation/dashboard.blade.php
+++ src/views/navigation/dashboard.blade.php
@@ -1,5 +1,5 @@
 
-<ul class="dashboard">
+<ul class="dashboard clearfix">
 	@foreach($nodes as $node)
 	<li class="dashboard-item">
 		<a href="{{ URL::route($node->page->alias, $node->params) }}"><span class="{{{ $node['icon_class'] }}}"></span><span>{{{ $node['title'] }}}</span></a>
